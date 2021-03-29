e32481425c6d788339e81201f9beae89f42ee2b5
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-29 23:04:44 +0200

    play with DOM

--- Basic/main.js
+++ Basic/main.js
@@ -13,6 +13,18 @@ console.log(newArr);
 //  create click event
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(e) {
-    console.log(e);
-    document.body.style.backgroundColor = 'red';
+    if (document.body.style.backgroundColor == 'white') {
+        document.body.style.backgroundColor = 'red'
+    } else {
+        document.body.style.backgroundColor = 'white'
+    }
 })
+
+
+for (let i = 0; i < newArr.length; i++) {
+    const newP = document.createElement('p');
+    newP.textContent = newArr[i];
+    newP.style.backgroundColor = 'green';
+    newP.style.color = 'white';
+    document.body.append(newP);
+}
