62028afc185f5bb697dba63ef872ca6776134253
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-29 22:54:44 +0200

    Selecting and Manipulating Elements

--- Basic/main.js
+++ Basic/main.js
@@ -1,7 +1,18 @@
-console.log('test');
-
 const arr = [1, 3, 4, 5, 6, 7];
 
+const newArr = [];
+
 for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+    // console.log(arr[i]);
+    newArr.push(arr[i] * 2);
 }
+
+console.log(newArr);
+
+
+//  create click event
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(e) {
+    console.log(e);
+    document.body.style.backgroundColor = 'red';
+})
