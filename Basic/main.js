1bbca2e35c2fb753a640bafd3918e1660ed5c29f
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-03-30 21:19:21 +0200

    add

--- Basic/main.js
+++ Basic/main.js
@@ -1,5 +1,7 @@
-const arr = [1, 3, 4, 5, 6, 7];
+'use strict';
 
+
+const arr = [1, 3, 4, 5, 6, 7];
 const newArr = [];
 
 for (let i = 0; i < arr.length; i++) {
@@ -13,6 +15,7 @@ console.log(newArr);
 //  create click event
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(e) {
+    // document.body.style.backgroundColor = 'red'
     if (document.body.style.backgroundColor == 'white') {
         document.body.style.backgroundColor = 'red'
     } else {
@@ -39,7 +42,16 @@ for (let i = 0; i < numbers.length; i++) {
 
 const btnChangeText = document.querySelector('#change-text');
 
-
 btnChangeText.addEventListener('click', function(e) {
     document.querySelector('p').textContent = 'Here is new Text'
+})
+
+
+const pickNumberBtn = document.querySelector('.pick-number');
+const addNumber = document.querySelector('.add-number').value;
+
+
+pickNumberBtn.addEventListener('click', function(e) {
+    newArr.push(addNumber)
+    console.log(newArr);
 })
