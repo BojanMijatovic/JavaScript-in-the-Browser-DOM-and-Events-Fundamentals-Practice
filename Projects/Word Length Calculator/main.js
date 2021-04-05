3705a7b6d987d28c3f2b32be944e8726ce78ace1
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-05 18:36:11 +0200

    add js logic

---
+++ Projects/Word Length Calculator/main.js
@@ -0,0 +1,13 @@
+let word = document.querySelector('.word').value;
+const wordLength = document.querySelector('.wordLength');
+
+const btnCalc = document.querySelector('.btn');
+
+btnCalc.addEventListener('click', function() {
+    wordLength.textContent = `Word have ${word.length}`;
+    if (word.length > 5) {
+        document.body.style.backgroundColor = 'red';
+    } else {
+        document.body.style.backgroundColor = 'teal';
+    }
+})
