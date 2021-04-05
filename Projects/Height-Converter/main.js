5c75445cfb23207974bea35b4ecd7ea99e1b13f1
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-05 09:47:17 +0200

    add css to DOM

--- Projects/Height-Converter/main.js
+++ Projects/Height-Converter/main.js
@@ -1,16 +1,11 @@
-const feetValue = document.querySelector('#feet').value;
+let feet = document.querySelector('#feet');
 /*
   get value with querySelector #ID-of-ELEMENT
 */
-const inchesValue = document.getElementById('inches').value;
-//   get value with getElementById  'ID-of-ELEMENT'
 
 const scoreText = document.querySelector('.text');
 //   get value with querySelector by classes  '.class-of-element'
 //  text content is string and we can change that in this way 
-
-
-
 
 
 //  add event listeners 
@@ -18,10 +13,12 @@ const convertBtn = document.querySelector('.btn');
 //  first add type of event here is CLICK , second argument is function
 // functions  is value  so we can use as arguments
 convertBtn.addEventListener('click', function(e) {
-    if (!feetValue && !inchesValue) {
+    if (!feet.value) {
         scoreText.textContent = `Insert real values`;
     } else {
-        scoreText.textContent = `Value of inches is ${parseInt(inchesValue)} and value of feet is ${parseInt(feetValue)}`
+        const cm = parseInt(feet.value) * 30.48;
+        scoreText.style.fontSize = '30px';
+        scoreText.textContent = `The value in cm is ${cm}`;
+        feet.value = '';
     }
-
 })
