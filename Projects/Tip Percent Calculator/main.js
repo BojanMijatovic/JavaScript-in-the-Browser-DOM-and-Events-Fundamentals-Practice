4303860c6703bccc5cf971f31438dd71d8054422
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-05 18:10:28 +0200

    Tip Percent Calculator

---
+++ Projects/Tip Percent Calculator/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const amount = document.getElementById('amount').value;
+const percent = document.getElementById('percent').value;
+const tip = document.getElementById('tip').value;
+
+const btnCalc = document.querySelector('.btn');
+
+btnCalc.addEventListener('click', function(e) {
+
+})
