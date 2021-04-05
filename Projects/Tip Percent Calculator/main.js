66b25aa2a98460a07b5e16e29350cf905056d751
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-05 18:20:47 +0200

    add new project

--- Projects/Tip Percent Calculator/main.js
+++ Projects/Tip Percent Calculator/main.js
@@ -1,11 +1,18 @@
 'use strict';
 
-const amount = document.getElementById('amount').value;
-const percent = document.getElementById('percent').value;
-const tip = document.getElementById('tip').value;
+const amount = document.getElementById('amount');
+const percent = document.getElementById('percent');
+const tip = document.getElementById('tip');
+const total = document.querySelector('.total');
 
 const btnCalc = document.querySelector('.btn');
 
 btnCalc.addEventListener('click', function(e) {
+    console.log(amount.value, percent.value);
 
+    const tipAmount = (amount.value / 100) * percent.value;
+    tip.textContent = `Your tip is ${tipAmount}$`;
+
+    const totalAmount = parseInt(amount.value) + tipAmount;
+    total.textContent = `Total is ${totalAmount}$`
 })
