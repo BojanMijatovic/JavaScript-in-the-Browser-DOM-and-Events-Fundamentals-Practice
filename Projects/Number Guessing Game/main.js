b907f5f8e3894b62aa3405a752368f98252b4b1e
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-05 17:56:08 +0200

    add text Content

--- Projects/Number Guessing Game/main.js
+++ Projects/Number Guessing Game/main.js
@@ -2,9 +2,13 @@
 
 const number = document.querySelector('.number');
 const submitBtn = document.querySelector('.submit');
+const previousGuess = document.querySelector('.previous-guess');
+const remain = document.querySelector('.remain-guess');
+const congratsValue = document.querySelector('.congrats');
 
-const remainAttempt = 10;
-const previousAttempt = 0;
+
+let remainAttempt = 10;
+let previousAttempt = 0;
 
 let randomNumber = Math.trunc(Math.random() * 10) + 1;
 
@@ -13,10 +17,16 @@ submitBtn.addEventListener('click', function() {
 
     if (parseInt(number.value) === randomNumber) {
         console.log('true');
-
+        congratsValue.textContent = 'Excellent!!!';
     } else if (parseInt(number.value) > randomNumber) {
         console.log(`To high`);
+        remain.textContent = `You have  ${remainAttempt--} previous`;
+        previousGuess.textContent = `You have  ${previousAttempt++} previous`;
+        console.log(remainAttempt, previousAttempt);
     } else if (parseInt(number.value) < randomNumber) {
         console.log(`To low`);
+        remain.textContent = `You have  ${remainAttempt--} previous`;
+        previousGuess.textContent = `You have  ${previousAttempt++} previous`;
+        console.log(remainAttempt, previousAttempt);
     }
 })
