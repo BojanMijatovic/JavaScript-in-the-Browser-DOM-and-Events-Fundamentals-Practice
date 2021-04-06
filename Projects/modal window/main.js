c298ac3b9b0b07964c9851c18e4961eb0ef1d0df
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-06 10:05:01 +0200

    modal window add

--- Projects/modal window/main.js
+++ Projects/modal window/main.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const openModalBtns = document.querySelectorAll('.show-modal-btn');
+const container = document.querySelector('.container');
+const closeContainerBtn = document.querySelector('.close');
+
+for (let i = 0; i < openModalBtns.length; i++) {
+    openModalBtns[i].addEventListener('click', function() {
+        // openModalBtns[i].classList.toggle('color');
+        modal.classList.remove('hidden');
+    })
+    closeModalBtn.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    })
+}
+
+
+
+closeContainerBtn.addEventListener('click', function() {
+    container.classList.add('hidden')
+})
