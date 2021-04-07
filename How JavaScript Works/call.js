fe879bccb1846147d5c69cb2c440a6c38fbfc609
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-07 12:52:52 +0200

    Scope Chain practice

--- How JavaScript Works/call.js
+++ How JavaScript Works/call.js
@@ -1,106 +1,25 @@
 'use strict';
-
-//  ------- Call Stack and Execution contest
-
-// Global execution contest
-const name = 'John';
-
-// const first = () => {
-//     let a = 1;
-//     const b = second(7, 8);
-//     a = a + b;
-//     return a;
-// }
-
-// function second(x, y) {
-//     var c = 2;
-//     return c;
-// }
-
-// const x = first()
-
-// console.log(x);
-
-
-//  --------- SCOPE CHAIN
-function show() {
-    const age = 44;
-
-    //  BLOCK SCOPE
-    if (age < 50) {
-        const test = 'Test';
-        var newJob = 'junior';
-
-        //  function can be called in here to
-        inside();
-    }
-
-    function inside() {
-        const job = 'front-end';
-        console.log(`${name} have ${job} in ${age} age `);
-    }
-    console.log(newJob);
-    //  var from IF BLOCK
-
-    newJob = 'janitor';
-    //  CALLING FUNCTION
-    inside();
-    console.log(newJob);
-}
-
-// show();
-
-//  SCOPE CHAIN VS CALL STACK
-
-// const a = 'John';
-// first();
-
-// function first() {
-
-//     const b = 'Hello';
-//     second();
-
-//     function second() {
-//         const c = 'Hi';
-//         third();
-//     }
-
-// }
-
-// function third() {
-//     const d = 'Hey';
-//     console.log(d + c + b + a);
-// }
-
-//  Practice scope
-
-function calcAge(birthYear) {
-    const age = 2021 - birthYear;
-
-    function printAge() {
-        const output = `You are born ${birthYear} now you have ${age}`
-        console.log(output);
-    }
-    printAge();
-    return age;
-}
+//  global scope
 const firstName = 'Alex';
-console.log(calcAge(1987));
-
-// g
-let a = "hello";
-// g
-first();
+let lastName = 'Smith';
 
 
-// local 
-function first() {
-    let b = "hi !";
-    second();
+//  block scope
+if (!false) {
+    //  function scope
+    function showFullName() {
+        const age = 44;
+        //  new const and value
+        const firstName = 'Ted';
+        //  new value
+        lastName = 'Test';
 
-    // local
-    function second() {
-        let c = "hey !";
-        console.log(a + b + c);
+        function calcAge() {
+            return 2021 - age;
+        }
+        const x = calcAge();
+        console.log(`${firstName} ${lastName} you are born ${x}`);
     }
+
+    showFullName();
 }
