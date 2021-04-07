bcf12a0524c7c9fbf4328ebc458000aeac5f8d50
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-07 13:15:47 +0200

    Scope Chain practice

--- How JavaScript Works/call.js
+++ How JavaScript Works/call.js
@@ -6,6 +6,9 @@ let lastName = 'Smith';
 
 //  block scope
 if (!false) {
+
+    showFullName();
+
     //  function scope
     function showFullName() {
         const age = 44;
@@ -15,11 +18,50 @@ if (!false) {
         lastName = 'Test';
 
         function calcAge() {
+            // const age = 140;
             return 2021 - age;
         }
         const x = calcAge();
         console.log(`${firstName} ${lastName} you are born ${x}`);
     }
-
-    showFullName();
 }
+
+
+const a = 4;
+const b = 2;
+
+function add() {
+
+    function moreNums() {
+        const a = 100;
+        return a + b + 10;
+    }
+
+    return moreNums() + 200;
+}
+
+console.log(add());
+
+
+//  ------
+
+let num1 = 10;
+let num2 = 10;
+
+function addNums() {
+    num2 = 100;
+    let a = num1;
+    a = num2;
+
+    //  function scope
+    function more() {
+        return num1 + num2 + a;
+    }
+
+    if (!false) {
+        return more();
+    }
+
+}
+
+console.log(addNums());
