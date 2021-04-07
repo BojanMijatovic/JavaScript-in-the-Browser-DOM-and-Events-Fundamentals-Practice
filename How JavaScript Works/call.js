67aceddb35046a85ac67d71f3bdb8eec364db239
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-07 09:48:00 +0200

    Scope and The Scope Chain

--- How JavaScript Works/call.js
+++ How JavaScript Works/call.js
@@ -1,19 +1,73 @@
 'use strict';
 
-//   Call Stack and Execution contest
+//  ------- Call Stack and Execution contest
 
+// Global execution contest
 const name = 'John';
 
-const first = () => {
-    let a = 1;
-    const b = second(7, 8);
-    a = a + b;
-    return a;
+// const first = () => {
+//     let a = 1;
+//     const b = second(7, 8);
+//     a = a + b;
+//     return a;
+// }
+
+// function second(x, y) {
+//     var c = 2;
+//     return c;
+// }
+
+// const x = first()
+
+// console.log(x);
+
+
+//  --------- SCOPE CHAIN
+function show() {
+    const age = 44;
+
+    //  BLOCK SCOPE
+    if (age < 50) {
+        const test = 'Test';
+        var newJob = 'junior';
+
+        //  function can be called in here to
+        inside();
+    }
+
+    function inside() {
+        const job = 'front-end';
+        console.log(`${name} have ${job} in ${age} age `);
+    }
+    console.log(newJob);
+    //  var from IF BLOCK
+
+    newJob = 'janitor';
+    //  CALLING FUNCTION
+    inside();
+    console.log(newJob);
 }
 
-function second(x, y) {
-    var c = 2;
-    return c;
+// show();
+
+//  SCOPE CHAIN VS CALL STACK
+
+const a = 'John';
+first();
+
+function first() {
+
+    const b = 'Hello';
+    second();
+
+    function second() {
+        const c = 'Hi';
+        third();
+    }
+
 }
 
-const x = first()
+function third() {
+    const d = 'Hey';
+    console.log(d + c + b + a);
+}
