bc15c8b52338b606818e1cdb3f6294bf8bad3c52
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-09 14:13:55 +0200

    loops example

--- Data Structures, Modern Operators and Strings/loops.js
+++ Data Structures, Modern Operators and Strings/loops.js
@@ -1,20 +1,19 @@
 'use strict';
-
 //  arr to copy in obj below
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //      LOOPS 
 
 // for-of LOOP
-for (const num of arr) {
-    if (num % 5 === 0) {
-        console.log(`Num is fizzbuzz`);
-    } else if (num % 3 === 0) {
-        console.log(`Num is buzz`);
-    } else if (num % 2 === 0) {
-        console.log(`Num is fizz`);
-    }
-}
+// for (const num of arr) {
+//     if (num % 5 === 0) {
+//         console.log(`Num is fizzbuzz`);
+//     } else if (num % 3 === 0) {
+//         console.log(`Num is buzz`);
+//     } else if (num % 2 === 0) {
+//         console.log(`Num is fizz`);
+//     }
+// }
 
 //  ES6 Enhanced Object Literals  
 
@@ -23,11 +22,12 @@ const person = {
     name: 'Alex',
     // insert arr like this
     arr,
-    addLastName() {}
+    thingToLearn: ['html', 'css', 'js'],
+    age: 34,
+    position: 'junior'
 }
 
-console.log(person);
-
+// console.log(person);
 
 // example for different way to select the property in an object
 const obj = {
@@ -45,12 +45,40 @@ const aa = 'a';
 
 //  via key names on object
 const names = Object.keys(obj);
+const values = Object.values(obj);
+
 for (const i of names) {
-    console.log(i);
+    console.log(`We have ${names.length} keys with values ${values}`);
 }
 
 //  via values on object
-const values = Object.values(obj);
+
 for (const v of values) {
-    console.log(v);
+    // console.log(v);
+}
+
+const keysPerson = Object.keys(person);
+const valuePerson = Object.values(person);
+
+const { thingToLearn, name, position } = person;
+// console.log(`${name} have to learn ${thingToLearn}`);
+
+
+const timeToLearn = function() {
+    const dayTime = 12;
+    const things = thingToLearn.length;
+    const breaks = 2.5;
+    const timeSpend = (dayTime - things) - breaks;
+    return `${timeSpend}h with breaks`;
+}
+
+
+const days = ['mon', 'thu', 'wen', 'thr', 'fri', 'str', 'sun'];
+
+for (const day of days) {
+    if (day === 'str' || day === 'sun') {
+        console.log(`${day} is for rest`);
+    } else {
+        console.log(`${day} is for work and learn ${timeToLearn()}`);
+    }
 }
