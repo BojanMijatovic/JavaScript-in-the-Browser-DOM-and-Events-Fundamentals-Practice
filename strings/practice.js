5aee03bd9b96d87cfab3cb9e65718fcd946fdace
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-12 20:49:41 +0200

    practice more strings and objects too

--- strings/practice.js
+++ strings/practice.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const person = {
     name: 'Alex',
     lastName: 'reDyy',
@@ -14,7 +16,7 @@ const person = {
             finished: false,
             technology: 'javaScript'
         }, {
-            instructor: 'Steven',
+            instructor: 'stephen',
             finished: false,
             technology: 'react'
         }
@@ -23,27 +25,67 @@ const person = {
     courses: 8
 }
 
-console.log(person);
+// console.log(person);
 
 const { lastName, hobby, technologies, thingToLearn: newThings, age } = person;
-console.log(lastName[0].toUpperCase() + lastName.toLowerCase().slice(1));
+// console.log(lastName[0].toUpperCase() + lastName.toLowerCase().slice(1));
 
 const [, , readBooks] = hobby;
-console.log(readBooks.split('_')[1].toLowerCase());
+// console.log(readBooks.split('_')[1].toLowerCase());
 
 const [max, andrew, steven] = technologies;
 const { instructor } = max;
-console.log(instructor.concat(' Swarmer'));
+// console.log(instructor.concat(' Schwarzmüller'));
+
+technologies.push({
+    instructor: 'Fillip',
+    finished: false,
+    technology: 'NEXT>JS'
+})
 
 for (const courses of technologies) {
     courses.numberOfTutorials = 3;
     if (courses.technology.includes('javaScript')) {
         courses.timeToPractice = true;
     }
-    console.log(courses);
 }
 
 newThings.push('css3', 'git', 'webpack');
-console.log(newThings);
+// console.log(newThings);
+// console.log(`${person.name} have ${person.age} years`);
 
-console.log(`${person.name} have ${person.age} years`);
+const check = function(symbol, ...courses) {
+    const [first, second, third] = courses;
+    let { technology } = third;
+    person.technologies[3].technology = technology.replace('>', '.').toLowerCase();
+}
+
+check(...technologies)
+
+// console.log(person);
+
+
+const bank = {
+    name: 'Intesa Bank',
+    users: [],
+    money: 200,
+}
+
+const addUser = function(userName, userLastName, userAmount = 0) {
+    const name = userName.toLowerCase();
+    const lastName = userLastName.toLowerCase();
+    const amount = userAmount
+    bank.users.push({
+        name,
+        lastName,
+        amount,
+        transactions: []
+    })
+}
+
+addUser('ted', 'terry')
+addUser('Phillip', 'Watson')
+addUser('Richard', 'Clarino', 1000)
+
+
+console.log(bank);
