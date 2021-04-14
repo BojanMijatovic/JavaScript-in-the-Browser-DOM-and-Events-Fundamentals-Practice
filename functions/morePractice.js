ebbde43762bc3cb87acca0612caf89ae90a977d4
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-14 12:40:41 +0200

    practice all

--- functions/morePractice.js
+++ functions/morePractice.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // IIFE
-
-const run = function() {
-    console.log(`Test`);
-}();
+// const run = function() {
+//     console.log(`Test`);
+// }();
 
 //  this is undefined 
 // console.log(run);
@@ -20,11 +19,10 @@ const secureBooking = function() {
     }
 }
 const booker = secureBooking();
-booker();
+// booker();
 
 
 let f = undefined;
-
 
 const g = function() {
     let test = 1;
@@ -34,23 +32,23 @@ const g = function() {
     return f;
 }
 
-g();
-f();
-f();
-f();
-
+// g();
+// f();
+// f();
+// f();
 
 
 (function() {
     const header = document.querySelector('h1');
     header.style.color = 'red';
     document.body.addEventListener('click', function() {
-        console.log(this);
+        // console.log(this);
         header.style.color = 'blue';
     })
 }())
 
 
+//      BIND METHOD
 
 const person = {
     name: 'Ted',
@@ -59,7 +57,7 @@ const person = {
     }
 }
 
-person.greet('Feeing');
+// person.greet('Feeing');
 
 
 const user = {
@@ -74,5 +72,69 @@ const greetUser = person.greet.bind(user);
 const welcome = person.greet.bind(newUser);
 
 
-welcome('Wood');
-greetUser('Redd');
+// welcome('Wood');
+// greetUser('Redd');
+
+
+//  practice 1
+const data = [];
+
+const userName = function(user) {
+    const [name, lastName] = user.split('|')
+    data.push({ name: name, lastName: lastName })
+    console.log(`User name is ${name} and password is ${lastName}`);
+}
+
+console.log(data);
+
+userName('pera|peric')
+userName('marko|markovic')
+
+//  practice 2 
+const calcStr = function(str1, str2) {
+    const firstStr = str1.slice(0, 2) + str1.slice(0, 2);
+    const secondStr = str2.slice(-3);
+    return console.log(`${firstStr}${secondStr}`);
+
+}
+
+calcStr('abcdef', 'ghijkl')
+
+
+//  practice 3 
+const acronym = function(str) {
+    const split = str.split(' ');
+    let newStr = ''
+    for (const word of split) {
+        newStr += word[0].toUpperCase();
+    }
+    console.log(newStr);
+}
+
+
+acronym('random access memory');
+
+//  example 4
+const car = {
+    mark: 'Ford',
+    model: 'Focus',
+    turnOn: false,
+    power: 120,
+    color: 'green',
+    startEngine() {
+        const start = this.turnOn = true;
+        return console.log(start);
+    },
+    stopEngine() {
+        const stop = this.turnOn = false;
+        return console.log(stop);
+    },
+    newColor(color) {
+        this.color = color;
+        return console.log(`${this.mark} have new color ${this.color}`);
+    }
+}
+
+car.newColor('blue');
+car.newColor('black');
+console.log(car);
