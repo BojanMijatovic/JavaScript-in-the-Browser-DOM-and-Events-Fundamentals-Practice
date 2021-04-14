6552b2c1939f980ef2807c3403fc428888cfb605
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-14 16:11:49 +0200

    Array Methods

--- Arrays/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Arrays/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -66,11 +66,57 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
+
+
+
+/////////////////////////////////////////////////
+
+//      METHODS
+
+//  start with simple array methods
+
+const arr = ['a', 'b', 'c', 'd', 'e', 'f']
+
+//  Slice Method
+//  this method will return new array and don't mutate old array
+console.log(arr.slice(2));
+//  end parameter is not included in new array
+
+//  Splice Method
+// second parameter is indicating the number of elements in the array to remove from start or first parameter.
+console.log(arr.splice(0, 3, 'Test', 'name', 'ana'));
+//  change original array 
+
+//  Reverse Method
+//method that reverse array , mutating the original, and returning a reference to the array.
+arr.reverse();
+
+//  Concat Method
+
+const arr2 = ['g', 'h', 't', 'i', 'p']
+
+// method is used to merge two or more arrays.This method does not change the existing arrays, but instead returns a new array.
+const letters = arr.concat(arr2);
+console.log(letters);
+
+//  Join Method
+// Specifies a string to separate each pair of adjacent elements of the array. The separator is converted to a string if necessary
+console.log(letters.join('-'));
+
+//  For Each Method
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+// High Order FUNCTION! forEach
+// method executes a provided function once for each array element. We have 3 parameters SINGLE ITEM, ITERATOR and ARRAY
+movements.forEach(function(movement, id) {
+    if (movement > 0) {
+        return console.log(`${id} Deposit ${movement}`);
+    } else {
+        return console.log(`${id} Withdraw ${movement}`);
+    }
+})
