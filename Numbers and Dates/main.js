91a5e8894f816acc3aa4a79471be04bdb96efad3
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-19 18:40:39 +0200

    Start with Numbers

---
+++ Numbers and Dates/main.js
@@ -0,0 +1,16 @@
+const num1 = 2;
+const num = 2.0;
+// console.log(num === num1);
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
+
+//  Type coercion
+// console.log(typeof(23 + ''));
+// console.log(+'23');
+
+
+
+//  Parse int and float
+
+let a = 1.3;
+// console.log(Number.parseFloat(a));
