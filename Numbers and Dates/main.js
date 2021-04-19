30f72bfaace81005ca7b52ca90b30a6e9b643a1a
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-19 21:28:34 +0200

    Start with Numbers

--- Numbers and Dates/main.js
+++ Numbers and Dates/main.js
@@ -4,7 +4,10 @@ const num = 2.0;
 // console.log(0.1 + 0.2 === 0.3);
 // console.log(0.1 + 0.2);
 
+
+
 //  Type coercion
+
 // console.log(typeof(23 + ''));
 // console.log(+'23');
 
@@ -14,3 +17,27 @@ const num = 2.0;
 
 let a = 1.3;
 // console.log(Number.parseFloat(a));
+
+
+
+//  MATH OBJECT
+
+// Math max, min, PI
+// round , trunc , ceil , floor , random , toFixed(2)
+
+
+// console.log(Math.max(1, 2, 355, 6, 7, 8, 9));
+// console.log(Math.min(1, 2, 355, 6, 7, 8, 9));
+
+
+// Remainder Operator
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arr.forEach((num, id) => {
+    if (num % 2 === 0) {
+        return console.log(`Num ${id + 1} is even`);
+    } else if (num % 1 === 0) {
+        return console.log(`Num ${id + 1} is odd`);
+    }
+})
