f52b592d1d2e5515a38f4cc49b3666dd4ec00bfa
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-21 14:26:32 +0200

    simple DOM ATM example

--- Projects/ATM/main.js
+++ Projects/ATM/main.js
@@ -1,6 +1,8 @@
+'use strict';
+
 const account = [{
     name: 'Alex',
-    deposit: 2000,
+    deposit: null,
     transactions: []
 }]
 
@@ -10,8 +12,9 @@ const amount = document.querySelector('.amount');
 const addAmountBtn = document.querySelector('.add-amount');
 const accountInfo = document.querySelector('.info');
 const userName = document.querySelector('.username');
+const sortTransactions = document.querySelector('.sort');
 const accountTransactions = document.querySelector('.account');
-
+const filteredBtn = document.querySelector('.filtered');
 
 addAmountBtn.addEventListener('click', () => {
     transactions.push(Number(amount.value));
@@ -20,23 +23,13 @@ addAmountBtn.addEventListener('click', () => {
 
     accountTransactions.innerHTML = '';
 
-
     // print status
-    transactions.forEach((money, id) => {
-        const moneyDiv = document.createElement('div');
-        moneyDiv.innerHTML = `<div>
-                                                <p>${id + 1} - ${money}$</p>
-                                              </div>`;
-        accountTransactions.append(moneyDiv);
-    })
+    printMoneyTransactions();
 
     //  reduce
-    const showTotal = () => {
-        const test = transactions.reduce((acc, current) => acc + current, 0)
-        return test;
-    }
+    const showTotal = () => transactions.reduce((acc, current) => acc + current, 0)
 
-
+    //  print info
     accountInfo.innerHTML = ''
     account.map(user => {
         const info = document.createElement('div');
@@ -45,4 +38,36 @@ addAmountBtn.addEventListener('click', () => {
           </div>`
         accountInfo.append(info);
     })
+})
+
+
+//  print transactions
+const printMoneyTransactions = () => {
+    transactions.forEach((money, id) => {
+        const moneyDiv = document.createElement('div');
+        moneyDiv.innerHTML = `<div>
+                                                <p>${id + 1} : ${money}$</p>
+                                              </div>`;
+        accountTransactions.append(moneyDiv);
+    })
+}
+
+
+//  sorting
+sortTransactions.addEventListener('click', () => {
+    transactions.sort((a, b) => a - b)
+    accountTransactions.innerHTML = '';
+    printMoneyTransactions(transactions);
+})
+
+
+
+//  filtered
+filteredBtn.addEventListener('click', () => {
+    const filterValue = Number(document.querySelector('.filter').value);
+    transactions.filter(money => money > filterValue).map(test => {
+        const testDiv = document.createElement('p');
+        testDiv.textContent = `Greater value from ${filterValue} is ${test}`;
+        return accountTransactions.append(testDiv)
+    });
 })
