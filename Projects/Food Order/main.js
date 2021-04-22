7e470cdec1acb8a19b4ee1ade826bf033f074c40
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-22 12:21:10 +0200

    simple DOM Food order

--- Projects/Food Order/main.js
+++ Projects/Food Order/main.js
@@ -5,43 +5,63 @@ const beerBtn = document.querySelector('.beer');
 const hamburgerBtn = document.querySelector('.hamburger');
 const totalBtn = document.querySelector('.total');
 const orderList = document.querySelector('.order-list');
+const totalAmount = document.querySelector('.totalAmount');
 
 const orders = [];
+
 const pizza = {
     name: 'pizza',
-    price: 40
+    price: 14
+}
+
+const beer = {
+    name: 'beer',
+    price: 4
 }
 
 
 
-let orderedItems = 0;
 
-const orderFood = (menuItem) => {
-    const orderTest = document.createElement('p');
-    orderTest.textContent = `You ordered ${menuItem.name} price is ${menuItem.price * (++orderedItems)}`;
 
-    orderList.append(orderTest);
+
+
+//  order
+const orderItem = (item, num) => {
     orders.push({
-        items: menuItem.name,
-        price: menuItem.price * (orderedItems)
+        orderedItem: item.name,
+        priceTotal: item.price * (num++)
     })
 }
+
+//  show 
+const showOrder = ((items) => {
+    items.forEach((item, id) => console.log(` ${id + 1} You have ${item.orderedItem} on price ${item.priceTotal}$`))
+})
 
 
 // pizza
 pizzaBtn.addEventListener('click', () => {
-    orderFood(pizza);
+    orderItem(pizza, 1);
     console.log(orders);
+    showOrder(orders)
+    console.log(calcTotal(orders));
 })
 
-//  total 
-totalBtn.addEventListener('click', () => {
-    orderList.innerHTML = '';
-    return orders.map(order => console.log(`You have ${order.items} and total is ${order.price}$`));
-})
+
 
 //  beer 
 beerBtn.addEventListener('click', () => {
-    const orderBeer = document.createElement('p');
+    orderItem(beer, 1);
+    console.log(orders);
+    showOrder(orders)
+    console.log(calcTotal(orders));
+})
 
+//  total 
+const calcTotal = (orders) => orders.reduce((acc, current) => acc + current.priceTotal, 0);
+
+console.log(calcTotal(orders));
+
+totalBtn.addEventListener('click', () => {
+    return console.log(`Your total is ${calcTotal(orders)}`);
 })
