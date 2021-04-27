0472c3f65d1014de9c72cae2030b2f5f841eaebf
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-04-27 18:57:46 +0200

    First AJAX Call: XMLHttpRequest

--- AJAX/src/main.js
+++ AJAX/src/main.js
@@ -12,3 +12,34 @@ navigator.geolocation.getCurrentPosition((position) => {
     // console.log(longitude, latitude);
 }
 */
+
+
+// First AJAX Call: XMLHttpRequest
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.eu/rest/v2/name/serbia');
+request.send();
+
+request.addEventListener('load', function() {
+    //  this refers to REQUEST
+    console.log(this.responseText);
+
+    //  convert to JSON
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const { name, nativeName, population } = data;
+    console.log(name, nativeName, population);
+})
+
+
+//  EXAMPLE FOR BEER
+const beer = new XMLHttpRequest();
+beer.open('GET', 'https://api.openbrewerydb.org/breweries?by_city=san_diego');
+beer.send();
+
+beer.addEventListener('load', () => {
+
+    const [data] = JSON.parse(beer.responseText);
+    console.log(data);
+})
